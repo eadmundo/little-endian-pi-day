f1ce226590b7d421c9a87fb4e1b03a4c2f6f0554
Edward Stone <edwardjstone@yahoo.com>
2015-06-17 21:01:44 +0100

    update travis deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+GITHUB_PATH="https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}"
 cd ${TRAVIS_BUILD_DIR}
 git config user.name "Travis CI"
 git config user.email "travis@littleendianpiday.org"
+git remote set-url --push origin $GITHUB_PATH
+git remote set-branches --add origin gh-pages
+git fetch -q
 git checkout gh-pages
 git ls-tree --name-only gh-pages | grep -v -e '.gitignore\|.nojekyll\|CNAME' | xargs rm -r
 cp dist/* .
 git add .
 git commit -m "Deployed from Travis CI"
-git push -f -q "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" origin gh-pages
+git push -f -q $GITHUB_PATH origin gh-pages
